3550442c862e848ccb0b510719a98cbaa6ae125e
Cole Johnson <colehjohn@gmail.com>
2017-11-07 10:56:09 -0500

    Updated to remote API

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.API_URL || 'http://localhost:8080/';
+const API_URL = 'https://plotvis-api.herokuapp,com' || process.env.API_URL || 'http://localhost:8080/';
 
 export { API_URL };
